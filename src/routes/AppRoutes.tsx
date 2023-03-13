ac82603b0643cc2fbe5d3f75a5e9161715d67d0f
DESKTOP-5H8EBD7\Asad <AsadXero@hotmail.com>
2023-03-13 12:43:16 +0500

    Terms And Conditions Page Done

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -13,6 +13,7 @@ import SignUpPage from '../pages/auth/sign-up/SignUpPage';
 import ForgotPasswordPage from '../pages/auth/forgot-password/ForgotPasswordPage';
 import OTPVerificationPage from '../pages/auth/otp-verification/OTPVerificationPage';
 import ResetPasswordPage from '../pages/auth/reset-password/ResetPasswordPage';
+import TermsAndConditionPage from '../pages/terms-and-conditions/TermsAndConditionPage';
 
 export const routeObjects: RouteObject[] = [
   {
@@ -35,34 +36,14 @@ export const routeObjects: RouteObject[] = [
     path: '/dashboard',
     element: <MainLayout />,
     children: [
-      {
-        index: true,
-        element: <Navigate to="home" replace />,
-      },
-      {
-        path: 'home',
-        element: <HomePage />,
-      },
-      {
-        path: 'orders',
-        element: <OrdersPage />,
-      },
-      {
-        path: 'payment-setting',
-        element: <PaymentSettingPage />,
-      },
-      {
-        path: 'delivery-address',
-        element: <DeliveryAddressPage />,
-      },
-      {
-        path: 'account',
-        element: <AccountPage />,
-      },
-      {
-        path: 'faqs',
-        element: <FAQSPage />,
-      },
+      { index: true, element: <Navigate to="home" replace /> },
+      { path: 'home', element: <HomePage /> },
+      { path: 'orders', element: <OrdersPage /> },
+      { path: 'payment-setting', element: <PaymentSettingPage /> },
+      { path: 'delivery-address', element: <DeliveryAddressPage /> },
+      { path: 'account', element: <AccountPage /> },
+      { path: 'faqs', element: <FAQSPage /> },
+      { path: 'terms-and-conditions', element: <TermsAndConditionPage /> },
     ],
   },
 ];
