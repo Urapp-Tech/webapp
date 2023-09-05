a1cd28018857710aa39773e07c79963d7a85f5d7
MuhammadAnas315 <139314566+MuhammadAnas315@users.noreply.github.com>
2023-09-05 11:56:04 +0500

    feature/LA-403-Order-detail-service-modified-in-webapp

--- src/services/Order.ts
+++ src/services/Order.ts
@@ -1,16 +1,16 @@
-import devNetwork from '../utilities/devNetwork';
+import devNetwork from '../utilities/devNetwork'
 
 export interface NewOrder {
-  cartId: string;
+  cartId: string
 }
 const addOrder = (data: NewOrder) => {
-  return devNetwork.post(`appOrder/newOrder`, data);
-};
+  return devNetwork.post(`appOrder/newOrder`, data)
+}
 const orderList = () => {
-  return devNetwork.get(`appOrder/list`);
-};
+  return devNetwork.get(`appOrder/webapp/list`)
+}
 
 export default {
   addOrder,
   orderList,
-};
+}
