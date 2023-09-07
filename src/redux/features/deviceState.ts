31472747730647a1895f8b6485c9e8723e073e41
MuhammadAnas315 <139314566+MuhammadAnas315@users.noreply.github.com>
2023-09-07 12:36:25 +0500

    feature/LA-435-Integrate-Delete-And-Active/Inactive-Service-In-Webapp

--- src/redux/features/deviceState.ts
+++ src/redux/features/deviceState.ts
@@ -1,44 +1,77 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { setItem } from '../../utilities/local-storage';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { setItem } from '../../utilities/local-storage'
+import Address from '../../services/Address'
 
 type DevicePayload = {
-  deviceId: string;
-  deviceType: string;
-  id: string;
-  isNotificationAllowed: boolean;
-  name: string;
-  tenant: string;
-  token: string;
-};
+  deviceId: string
+  deviceType: string
+  id: string
+  isNotificationAllowed: boolean
+  name: string
+  tenant: string
+  token: string
+}
 
 type DeviceState = {
-  DeviceData: DevicePayload | null;
-  Address: string;
-  AddressList: [];
-};
+  DeviceData: DevicePayload | null
+  Address: string
+  AddressList: []
+}
 const initialState: DeviceState = {
   DeviceData: null,
   Address: '',
   AddressList: [],
-};
+}
 export const deviceStateSlice: any = createSlice({
   name: 'device',
   initialState,
   reducers: {
     setDeviceData: (state, action: PayloadAction<DevicePayload>) => {
-      state.DeviceData = action.payload;
-      setItem('deviceData', JSON.stringify(action.payload));
+      state.DeviceData = action.payload
+      setItem('deviceData', JSON.stringify(action.payload))
     },
     setUserAddress: (state, action: PayloadAction<string>) => {
-      state.Address = action.payload;
+      state.Address = action.payload
+    },
+    setUserNewAddress: (state: any, action: PayloadAction<any>) => {
+      state.AddressList = [...state.AddressList, action.payload]
     },
     setUserAddressList: (state, action: PayloadAction<[]>) => {
-      setItem('Address', action.payload);
-      state.AddressList = action.payload;
+      setItem('Address', action.payload)
+      state.AddressList = action.payload
+    },
+    setAddressStatus: (state: any, action: PayloadAction<any>) => {
+      const addressList = state.AddressList || []
+      state.AddressList = addressList.map((el: any) => {
+        if (el.id === action.payload.id) {
+          return {
+            ...el,
+            isActive: true,
+          }
+        } else if (el.isActive) {
+          return {
+            ...el,
+            isActive: false,
+          }
+        } else {
+          return el
+        }
+      })
+    },
+    deleteUserAddress: (state: any, action: PayloadAction<any>) => {
+      state.AddressList = state.AddressList.filter(
+        (el: any) => el.id !== action.payload.id,
+      )
     },
   },
-});
+})
 
-export const { setDeviceData, setUserAddress, setUserAddressList } =
-  deviceStateSlice.actions;
-export default deviceStateSlice.reducer;
+export const {
+  setDeviceData,
+  setUserAddress,
+  setUserAddressList,
+  setAddressStatus,
+  setUserNewAddress,
+  deleteUserAddress,
+} = deviceStateSlice.actions
+export default deviceStateSlice.reducer
--- src/services/Address.ts
+++ src/services/Address.ts
@@ -1,21 +1,30 @@
-import devNetwork from '../utilities/devNetwork';
+import devNetwork from '../utilities/devNetwork'
 
 export interface AddressPayload {
-  name: string;
-  address: string;
-  type: string;
-  latitude: number;
-  longitude: number;
+  name: string
+  address: string
+  type: string
+  latitude: number
+  longitude: number
 }
 
 const getUserAddress = () => {
-  return devNetwork.get('appUserAddress/list');
-};
+  return devNetwork.get('appUserAddress/list')
+}
 const userAddress = (data: AddressPayload) => {
-  return devNetwork.post(`appUserAddress/add`, data);
-};
+  return devNetwork.post(`appUserAddress/add`, data)
+}
+
+const UpdateAddressStatus = (tenantId: string, id: string) => {
+  return devNetwork.put(`appUserAddress/update/status/${tenantId}/${id}`, {})
+}
+const deleteUserAddress = (tenantId: string, id: string) => {
+  return devNetwork.delete(`appUserAddress/delete/${tenantId}/${id}`)
+}
 
 export default {
   userAddress,
   getUserAddress,
-};
+  UpdateAddressStatus,
+  deleteUserAddress,
+}
