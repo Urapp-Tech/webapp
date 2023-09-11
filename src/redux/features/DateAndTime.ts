6723aa9dbcada7d9d6356ffeb387789d5ab8dc51
MuhammadAnas315 <139314566+MuhammadAnas315@users.noreply.github.com>
2023-09-11 16:10:15 +0500

    bug/LA-413-Inaccurate-Display-of-Chosen-Date-After-Making-Selections-in-web-app

--- src/redux/features/DateAndTime.ts
+++ src/redux/features/DateAndTime.ts
@@ -1,28 +1,28 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import dayjs, { Dayjs } from 'dayjs';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import dayjs, { Dayjs } from 'dayjs'
 
 interface DateState {
-  DropOff: Dayjs | null;
-  PickUp: Dayjs | null;
+  DropOff: Dayjs | null
+  PickUp: Dayjs | null
 }
 
 const initialState: DateState = {
   DropOff: null,
   PickUp: null,
-};
+}
 
 export const DateSliceReducer = createSlice({
   name: 'date',
   initialState,
   reducers: {
     setPickup: (state: DateState, action: PayloadAction<Dayjs | null>) => {
-      state.PickUp = action.payload;
+      state.PickUp = action.payload
     },
     setDropOff: (state: DateState, action: PayloadAction<Dayjs | null>) => {
-      state.DropOff = action.payload;
+      state.DropOff = action.payload
     },
   },
-});
+})
 
-export const { setPickup, setDropOff } = DateSliceReducer.actions;
-export default DateSliceReducer.reducer;
+export const { setPickup, setDropOff } = DateSliceReducer.actions
+export default DateSliceReducer.reducer
