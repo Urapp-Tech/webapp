fd9f946c44823e217eb53a2db15f644dd15cf4e3
Syed Taqiuddin <34369865+taqiuddin6987@users.noreply.github.com>
2024-05-08 17:02:46 +0500

    config file is updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
   plugins: [
     react(),
-    VitePWA({ registerType: 'autoUpdate', devOptions: { enabled: true } }),
+    // VitePWA({ registerType: 'autoUpdate', devOptions: { enabled: true } }),
   ],
   css: {
     modules: {
