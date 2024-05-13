cc8e35fbef67888b1597649e405b9f001a22c650
Shoaib Ahmed <shoaib@urapptech.com>
2024-05-13 11:40:52 +0500

    Remove unused assets and images

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -10,7 +10,6 @@ import paymentSettingsCss from './css/payment-settings.css';
 import sidebarCss from './css/sidebar.css';
 import topbarCss from './css/topbar.css';
 
-import bubbleBackground from './images/bubble-background.png';
 import facebook from './images/facebook.svg';
 import forgotPassImage from './images/forgotPass-screen-img.svg';
 import googleMapMarker from './images/google-map-marker.png';
@@ -24,30 +23,7 @@ import visa from './images/logo-visa.svg';
 import logo from './images/logo.svg';
 import registerImage from './images/register-screen-img.svg';
 import speechBubble from './images/speech-bubble.png';
-import logoBlack from './images/ur-laundry-logo-black.svg';
-import logoWhite from './images/ur-laundry-logo-white.svg';
 
-import clothes from './temp-images/clothes.png';
-import frocks1 from './temp-images/frocks-1.png';
-import itemPlaceholderImage from './temp-images/item-placeholder-image.png';
-import jacket1 from './temp-images/jacket-1.png';
-import curtain from './temp-images/main-cat-img-commercial.svg';
-import dryCleaning from './temp-images/main-cat-img-dry-cleaning.svg';
-import home from './temp-images/main-cat-img-home.svg';
-import ironing from './temp-images/main-cat-img-iron.svg';
-import jacket from './temp-images/main-cat-img-outdoor-wear.svg';
-import laundry from './temp-images/main-cat-img-wash-and-fold.svg';
-import map from './temp-images/map.png';
-import neckScarf1 from './temp-images/neck-scarf-1.png';
-import neckScarf2 from './temp-images/neck-scarf-2.png';
-import pants1 from './temp-images/pants-1.png';
-import pants from './temp-images/pants.png';
-import printedTShirt1 from './temp-images/printed-t-shirt-1.png';
-import printedTShirt2 from './temp-images/printed-t-shirt-2.png';
-import shirt1 from './temp-images/shirt-1.png';
-import shirt2 from './temp-images/shirt-2.png';
-import shirt from './temp-images/shirt.png';
-import wash from './temp-images/wash.png';
 import bgLogin from './images/bg-login.png';
 import forgotBg from './images/forgot-bg.png';
 import envelopeMsg from './images/envelope-msg.png';
@@ -70,7 +46,6 @@ const assets = {
     topbarCss,
   },
   images: {
-    bubbleBackground,
     card,
     cash,
     facebook,
@@ -79,8 +54,6 @@ const assets = {
     iconMap,
     loginImage,
     logo,
-    logoBlack,
-    logoWhite,
     mastercard,
     paypal,
     registerImage,
@@ -92,29 +65,6 @@ const assets = {
     otpMSg,
     keyIcon,
     Star,
-  },
-  tempImages: {
-    clothes,
-    curtain,
-    dryCleaning,
-    frocks1,
-    home,
-    ironing,
-    itemPlaceholderImage,
-    jacket,
-    jacket1,
-    laundry,
-    map,
-    neckScarf1,
-    neckScarf2,
-    pants,
-    pants1,
-    printedTShirt1,
-    printedTShirt2,
-    shirt,
-    shirt1,
-    shirt2,
-    wash,
   },
 };
 
